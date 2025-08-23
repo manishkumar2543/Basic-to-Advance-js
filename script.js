2cc15c814c6e95f73bc11aee8766f5bb1906bba4
Manish kumar <169014705+manishkumar2543@users.noreply.github.com>
2025-08-23 11:22:33 +0530

    Add files via upload
    Loop and flow control basic type

---
+++ script.js
@@ -0,0 +1,168 @@
+// window me add hota 
+// function scoped hota h 
+//  Hm ise fir se delceare kr sakte same nam se hme error dekhne ko nhim milega.
+
+// var a= 12;
+// var a =13;
+// console.log(a);
+
+
+// vhi hme let a= 12 ke case me error dekhene ko milega
+// let a=12;
+// let a=12;
+// console.log(a)
+
+
+// function example() {
+//   var x = 10;
+// //   console.log(x); // ✅ 10
+// console.log(x)
+// }
+// console.log(x); // ❌ Error: x is not d
+
+
+
+//Write a program that takes a number as input and prints whether it is even or odd.
+
+
+
+
+// 1. Positive, Negative, or Zero
+// Take a number and check if it’s positive, negative, or zero.
+
+// let number= prompt(("enter a number"));
+
+// if(number > 0){
+//     console.log("positve number");
+// }
+// else if(number < 0){
+//     console.log("negative number")
+// }
+// else{
+//     console.log("zero number")
+// }
+
+
+
+// Maximum of Two Numbers
+// Take two numbers and print which one is greater (or if they are equal).
+// let num1 = prompt("enter first number");
+// let num2=prompt("enter second number");
+// if(num1 > num2){
+//     console.log("greater number" +num1);
+// }
+// else if(num1 < num2){
+//     console.log("grater number" +num2)
+// }
+// else{
+//     console.log("both are equal")
+// }
+
+
+// 4. Age Group
+// Take someone’s age and print:
+// "Child" if under 13
+// "Teen" if 13–19
+// "Adult" if 20–59
+// "Senior" if 60 or older
+
+// let age = prompt ("enter your age");
+//  if(age < 13){
+//     console.log("Child");
+//  }
+//  else if(age >=13 && age <=19 ){
+//     console.log("Teen")
+//  }
+// else if(age >=20 && age <=59){
+//     console.log("Adult")
+// }
+// else{
+//     console.log("senior")
+// }
+
+
+
+// 5. Pass or Fail
+// Take a student's percentage and print "Pass" if ≥ 40, else "Fail".
+
+// let percentage =prompt("enter your percentage");
+// if(percentage >= 40){
+//     console.log("Pass")
+// }
+// else{
+//     console.log("Fail")
+// }
+
+// 6. Sign-in System
+// Take username and password as input;
+//  print "Login Successful" if both match predefined
+//   values, else "Invalid Credentials"
+
+
+// let username =prompt("enter your username");
+// let password =prompt("enter your password");
+
+// let predefinedUsername="admin";
+// let predefinedPassword="12345";
+// if(username === predefinedUsername && password === predefinedPassword){
+//     console.log("Login Successful");
+// }
+// else{
+//     console.log("Invalid Credentials");
+// }
+
+
+
+// 7. Traffic Light
+// Take a color ("red", "yellow", "green") 
+// and print the correct action ("Stop", "Ready", "Go").
+
+// let color=prompt("enter a color (red, yellow, green)");
+
+// if(color === "red"){
+//     console.log("Stop");
+// }
+// else if(color === "yellow"){
+//     console.log("Ready")
+// }
+// else if(color === "green"){
+//     console.log("GO")
+// }
+// else{
+//     console.log("Invalid color");
+// }
+
+
+// 8. Number Range Check
+// Check if a number is between 10 and 50 (inclusive).
+
+// let number =parseFloat(prompt("enter a number"));
+// if(number >=10 && number <=50){
+//     console.log("Number is between 10 and 50");
+// }
+// else{
+//     console.log("Number is not between 10 and 50")
+// }
+
+
+
+// function fn(){
+//     console.log("hello")
+// }
+// fn();
+
+
+// Ya phir named function expression bhi possible hai:🏓
+// let fn=function(){
+//     console.log("hey")
+// }
+// fn();
+
+
+
+
+
+
+
+
+
